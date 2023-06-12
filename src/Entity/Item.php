8e1ee1774c5a957e070209254b5b1072927d4260
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-06-12 16:24:17 +0200

    item crud + form + hero update

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -6,6 +6,7 @@ use App\Repository\ItemRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ItemRepository::class)
@@ -20,6 +21,7 @@ class Item
     private $id;
 
     /**
+     * @Assert\NotBlank(message="Le champ Nom de l'objet ne peut pas être vide")
      * @ORM\Column(type="string", length=255)
      */
     private $name;
@@ -31,35 +33,42 @@ class Item
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(type="numeric", message="Le champ Santé doit être un nombre")
      */
     private $health;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Force doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $strength;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Intelligence doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $intelligence;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Dextérité doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $dexterity;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Défense doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $defense;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Karma doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $karma;
 
     /**
+     * @Assert\Type(type="numeric", message="Le champ Expérience doit être un nombre")
      * @ORM\Column(type="integer", nullable=true)
      */
     private $xp;
@@ -245,5 +254,10 @@ class Item
         }
 
         return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
     }
 }
---
+++ src/Form/ItemType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Item;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ["label" => "Nom de l'objet"])
+            ->add('picture', TextType::class, ["label" => "Image de l'objet"])
+            ->add('health', NumberType::class, ["label" => "Santé"])
+            ->add('strength', NumberType::class, ["label" => "Force"])
+            ->add('intelligence', NumberType::class, ["label" => "Intelligence"])
+            ->add('dexterity', NumberType::class, ["label" => "Dextérité"])
+            ->add('defense', NumberType::class, ["label" => "Défense"])
+            ->add('karma', NumberType::class, ["label" => "Karma"])
+            ->add('xp', NumberType::class, ["label" => "Expérience"])
+            // ->add('heroes')
+            // ->add('npcs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Item::class,
+            "attr" => ["novalidate" => 'novalidate']
+        ]);
+    }
+}
