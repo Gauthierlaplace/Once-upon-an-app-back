60d6e17d6dfbe1cff5efe317da572d1429f9fd11
Pierre Salhab <pierre_0613@hotmail.fr>
2023-06-13 16:06:24 +0200

    api user add route created

--- src/Controller/api/UserApiController.php
+++ src/Controller/api/UserApiController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller\api;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserApiController extends CoreApiController
 {
@@ -49,6 +54,46 @@ class UserApiController extends CoreApiController
     // TODO Edit one user
 
     // TODO Add one user
+
+    /**
+     * ajout de film
+     *
+     * @Route("/api/users",name="app_api_users_add", methods={"POST"})
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param UserRepository $userRepository
+     */
+    public function add(
+        Request $request,
+        UserRepository $userRepository,
+        SerializerInterface $serializer,
+        ValidatorInterface $validatorInterface
+    ) {
+        // Récupérer le contenu JSON
+        $jsonContent = $request->getContent();
+        // Désérialiser (convertir) le JSON en entité Doctrine User
+        try { // on tente de désérialiser
+            $user = $serializer->deserialize($jsonContent, User::class, 'json');
+        } catch (Exception $exception){
+            // Si on n'y arrive pas, on passe ici
+            // dd($exception);
+            // code 400 ou 422
+            return $this->json("JSON Invalide", Response::HTTP_BAD_REQUEST);
+        }
+
+        // on valide les données de notre entité
+        $errors = $validatorInterface->validate($user);
+        // Y'a-t-il des erreurs ?
+        if (count($errors) > 0) {
+            return $this->json($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // On sauvegarde l'entité
+        $userRepository->add($user, true);
+
+        return $this->json201($user, ["user_create"]);
+    }
 
     // TODO Delete one user
 }
