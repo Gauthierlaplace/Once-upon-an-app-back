bee051b82ae90cf1c19e49ccea47483dd1ab9f2e
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-06-14 15:46:47 +0200

    Upgrade Route api/login_check with current User Data

--- src/Controller/api/UserApiController.php
+++ src/Controller/api/UserApiController.php
@@ -14,43 +14,35 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserApiController extends CoreApiController
 {
+    /**
+     * Route giving CurrentUser details from JWT Token 
+     * 
+     * @Route("/api/users/details", name="user_details", methods={"GET"})
+     */
+    public function currentUser(): JsonResponse
+    {
+        /** @var App\Entity\User $user */
+        $user = $this->getUser();
 
-    // TODO Connexion (useless)
-    // /**
-    //  * @Route("/api/users/{partialEmail}", name="app_api_users", methods={"GET"})
-    //  */
-    // public function connectUser($partialEmail, UserRepository $userRepository, Request $request)
-    // {
-    //     $partialEmail = $request->attributes->get('partialEmail');
+        // Vérifiez si l'utilisateur est authentifié
+        if (!$user) {
+            return new JsonResponse(['message' => 'Utilisateur non authentifié.'], 401);
+        }
+        // dd($user);
+        // Récupérez les détails de l'utilisateur connecté
+        $userId = $user->getId();
+        $email = $user->getEmail();
+        $pseudo = $user->getPseudo();
+        $avatar = $user->getAvatar();
 
-    //     $user = $userRepository->findOneByEmail($partialEmail);
-
-    //     if ($user === null) {
-    //         return $this->json404(["message" => "Aucun utilisateur trouvé !"]);
-    //     }
-
-    //     return $this->json200($user, ["user_connect"]);
-
-    //     // TODO trouver comment sécurisé la requete (2 emails presque identiques)
-
-    // }
-
-    // TODO Read one user (useless)
-    // /**
-    //  * @Route("/api/user/{id}", name="app_api_user_read", requirements={"id"="\d+"}, methods={"GET"})
-    //  */
-    // public function read($id, UserRepository $userRepository): JsonResponse
-    // {
-    //     $user = $userRepository->find($id);
-    //     // gestion 404
-    //     if ($user === null) {
-    //         return $this->json404(["message" => "Cet utilisateur n'existe pas"]);
-    //     }
-
-    //     return $this->json200($user, ["user_read"]);
-    // }
-
-    // TODO Edit one user
+        // Retournez les détails de l'utilisateur au format JSON
+        return new JsonResponse([
+            'id' => $userId,
+            'email' => $email,
+            'pseudo' => $pseudo,
+            'avatar' => $avatar,
+        ]);
+    }
 
     // TODO Add one user
     /**
@@ -93,37 +85,10 @@ class UserApiController extends CoreApiController
         return $this->json201($user, ["user_create"]);
     }
 
+    // TODO Edit one user
+    
     // TODO Delete one user
 
 
-    // TODO CurrentUser details from JWT Token    (DONE)
-    /**
-     * Route giving CurrentUser details from JWT Token 
-     * 
-     * @Route("/api/users/details", name="user_details", methods={"GET"})
-     */
-    public function currentUser(): JsonResponse
-    {
-        /** @var App\Entity\User $user */
-        $user = $this->getUser();
-
-        // Vérifiez si l'utilisateur est authentifié
-        if (!$user) {
-            return new JsonResponse(['message' => 'Utilisateur non authentifié.'], 401);
-        }
-        // dd($user);
-        // Récupérez les détails de l'utilisateur connecté
-        $userId = $user->getId();
-        $email = $user->getEmail();
-        $pseudo = $user->getPseudo();
-        $avatar = $user->getAvatar();
-
-        // Retournez les détails de l'utilisateur au format JSON
-        return new JsonResponse([
-            'id' => $userId,
-            'email' => $email,
-            'pseudo' => $pseudo,
-            'avatar' => $avatar,
-        ]);
-    }
+    
 }
---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+
+  /**
+   * Data user directly from JWT Token (route api/login_check)
+   * 
+   * @param AuthenticationSuccessEvent $event
+   */
+  public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+  {
+    $data = $event->getData();
+    $user = $event->getUser();
+
+    if (!$user instanceof User) {
+      return;
+    }
+
+    $data['data'] = array(
+      'id' => $user->getId(),
+      'email' => $user->getEmail(),
+      'pseudo' => $user->getPseudo(),
+      'avatar' => $user->getAvatar(),
+    );
+
+    $event->setData($data);
+  }
+}
