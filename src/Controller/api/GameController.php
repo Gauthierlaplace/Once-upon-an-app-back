91bbcd8c54758d60ed89825a0805632e061b3171
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-06-15 14:46:02 +0200

    strategy gameplay btnPlay v1

--- src/Controller/api/GameController.php
+++ src/Controller/api/GameController.php
@@ -15,13 +15,20 @@ class GameController extends AbstractController
     {
         // TODO Arrivée Event Départ du Biome 1 , 1er noeud de choix d'ending
         // un random EventA Départ + opening de 2 random Event(BetC)de event_type issue de la table ending de l' EventA
+        
+        // TODO Récupérer l'EventA($eventA) Event : Départ Biome 1 (`SELECT `title` FROM `event` WHERE `title` === "Départ Biome 1"` )
 
+        // TODO On a besoin de l'ID de EventA pour aller chercher les Endings correspondant 
+        // $eventAId
 
+        // TODO Des x event_typeId issue des endings de l'eventA, on en prend 2 en random event_type
+        // Random mysql ou Dsql 
 
+        // TODO Une fois les 2 random event_type pris, faire un random par event_type de Biome correspondant pour avoir l'id EventB et l'id EventC
 
+        // TODO De l'eventB et l'eventC stocker l'opening de chacun (BTN choice next Event (B ou C))
 
-
-
+        // TODO Nouvelle route nécessaire pour la suite, donc nouvelle fonction :)
 
 
 
