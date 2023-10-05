71f0afc799a7b8c4c189202d08a897e65e4dbde4
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-10-05 16:19:20 +0200

    building function to find attack or not succeed

--- src/Controller/api/FightController.php
+++ src/Controller/api/FightController.php
@@ -2,28 +2,90 @@
 
 namespace App\Controller\api;
 
+use App\Repository\EffectRepository;
+use App\Repository\HeroRepository;
+use App\Repository\NpcRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FightController extends CoreApiController
 {
     /**
-     * @Route("/api/event/fight/attack/{id}", name="app_api_attack" , requirements={"id"="\d+"}, methods={"GET"})
+     * @Route("/api/event/fight/{npcId}/attack/{effectId}", name="app_api_attack" , requirements={"effectId"="\d+", "npcId"="\d+"}, methods={"GET"})
      */
-    public function attackOrMiss(): JsonResponse
+    public function attackOrMiss($effectId, $npcId, NpcRepository $npcRepository, EffectRepository $effectRepository, HeroRepository $heroRepository): JsonResponse
     {
         /** @var App\Entity\User $user */
         $user = $this->getUser();
 
-        // Hero vie 20 de base
-        // Npc vie 10 de base
-        
+        $heroesCollection = $user->getHeroes();
+        $heroes = $heroesCollection->toArray();
+
+        $arrayHero = [];
+        foreach ($heroes as $hero) {
+            $arrayHero = [
+                'heroId' => $hero->getId(),
+                'heroName' => $hero->getName(),
+                'heroMaxHealth' => $hero->getMaxHealth(),
+                'heroHealth' => $hero->getHealth(),
+                'heroStrength' => $hero->getStrength(),
+                'heroIntelligence' => $hero->getIntelligence(),
+                'heroDexterity' => $hero->getDexterity(),
+                'heroDefense' => $hero->getDefense(),
+                'heroKarma' => $hero->getKarma(),
+            ];
+        }
+
+        $npc = $npcRepository->find($npcId);
+
+        $arrayNpc = [
+            'npcId' => $npc->getId(),
+            'npcName' => $npc->getName(),
+            'npcMaxHealth' => $npc->getMaxHealth(),
+            'npcHealth' => $npc->getHealth(),
+            'npcStrength' => $npc->getStrength(),
+            'npcIntelligence' => $npc->getIntelligence(),
+            'npcDexterity' => $npc->getDexterity(),
+            'npcDefense' => $npc->getDefense(),
+            'npcKarma' => $npc->getKarma(),
+        ];
+
+        $effect = $effectRepository->find($effectId);
+
+        $arrayEffect = [
+            'effectId' => $effect->getId(),
+            'effectName' => $effect->getName(),
+            'effectDescription' => $effect->getDescription(),
+            'effectHealth' => $effect->getHealth(),
+            'effectStrength' => $effect->getStrength(),
+            'effectIntelligence' => $effect->getIntelligence(),
+            'effectDexterity' => $effect->getDexterity(),
+            'effectDefense' => $effect->getDefense(),
+            'effectKarma' => $effect->getKarma(),
+            'effectXp' => $effect->getXp(),
+        ];
+
         // TODO 
-        //! Sauvegarder la Vie Max du NPC (propriété de table à créer maxHealth)
         //* Déterminer qui commencera le combat en fonction de l'id de l'effet
-        // ! id = id de l'effet
         // si l'effet est positif on influe sur la vie du NPC, le Npc attaque en premier
         // si l'effet est négatif on influe sur la vie du Player, le Player attaque en premier
+        $effectHealth = $effect->getHealth();
+        // dump($effectHealth);
+        $heroCurrentHealth = $arrayHero['heroHealth'];
+        $npcCurrentHealth = $arrayNpc['npcHealth'];
+        
+        if ($effectHealth <= 0) {
+            // on tape le Héro et on le MAJ en BDD
+            $hero = $heroRepository->find($arrayHero['heroId']);
+            $arrayHero['heroHealth'] = $hero->setHealth($heroCurrentHealth + $effectHealth);
+        } else {
+            // on tape le npc et on MAJ en BDD
+            $npc = $npcRepository->find($arrayNpc['npcId']);
+            
+            $arrayNpc['npcHealth'] = $npc->setHealth($npcCurrentHealth - $effectHealth);
+        }
+        
+        // dd($arrayNpc['npcHealth']);
 
 
         //! L'attaquant $attacker doit déterminer s'il touche ou pas 
@@ -52,12 +114,15 @@ class FightController extends CoreApiController
 
 
 
-
-
         $data = [
-            'player' => $hero,
-
+            'hero' => $arrayHero,
+            'npc' => $arrayNpc,
+            'effect' => $arrayEffect,
+            'attacker' => "hero ou npc",
+            'defender' => "hero ou npc",
         ];
-        return $this->json200($data, ["fight"]);
+
+
+        return $this->json200($data, ["game"]);
     }
 }
