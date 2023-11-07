e70ff7f0634670ce3ff3582e3e3ba8fa3c469808
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-11-07 15:47:20 +0100

    itemTest setting

---
+++ tests/Entity/itemTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Item;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class itemTest extends KernelTestCase
+{
+    protected static function getKernelClass()
+    {
+        return \App\Kernel::class;
+    }
+
+    public function getEntity(): Item
+    {
+        return (new Item())->setName('Item #1')
+                    ->setHealth(100)
+                    ->setStrength(100)
+                    ->setIntelligence(100)
+                    ->setDexterity(100)
+                    ->setDefense(100)
+                    ->setKarma(100)
+                    ->setXp(100);
+    }
+
+    public function testEntityIsValid(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $item = $this->getEntity();
+
+        $errors = $container->get('validator')->validate($item);
+        $this->assertCount(0, $errors);
+    }
+}
