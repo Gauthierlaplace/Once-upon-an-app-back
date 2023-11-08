c204aaa7051eccf027237074a17bf6b95df39200
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-11-08 08:37:55 +0100

    Some more tests on ending and race entities

--- tests/Entity/EndingTest.php
+++ tests/Entity/EndingTest.php
@@ -3,7 +3,8 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Ending;
-use App\Repository\EventRepository;
+use App\Entity\Event;
+use App\Entity\EventType;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EndingTest extends KernelTestCase
@@ -16,17 +17,17 @@ class EndingTest extends KernelTestCase
     public function getEntity(): Ending
     {
         $container = static::getContainer();
-        // Récupérez l'EntityManager
         $entityManager = $container->get('doctrine')->getManager();
-
-        // Utilisez l'EntityManager pour obtenir l'EventRepository
+        
         $eventRepository = $entityManager->getRepository(Event::class);
-
-        // Maintenant, vous pouvez utiliser le EventRepository
         $event = $eventRepository->find(3);
 
+        $eventTypeRepository = $entityManager->getRepository(EventType::class);
+        $eventType = $eventTypeRepository->find(1);
+
         return (new Ending())->setContent('Ending #1')
-                             ->setEvent($event);
+                             ->setEvent($event)
+                             ->setEventType($eventType);
     }
 
     public function testEntityIsValid(): void
--- tests/Entity/RaceTest.php
+++ tests/Entity/RaceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\Npc;
 use App\Entity\Race;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -14,8 +15,14 @@ class RaceTest extends KernelTestCase
 
     public function getEntity(): Race
     {
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $npcRepository = $entityManager->getRepository(Npc::class);
+        $npc = $npcRepository->find(10);
+
         return (new Race())->setName('Race #1')
-                    ->setDescription('Description #1');
+            ->setDescription('Description #1')
+            ->addNpc($npc);
     }
 
     public function testEntityIsValid(): void
@@ -28,5 +35,44 @@ class RaceTest extends KernelTestCase
         $errors = $container->get('validator')->validate($entity);
         $this->assertCount(0, $errors);
     }
+
+    public function testEntityGetters(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $raceRepository = $entityManager->getRepository(Race::class);
+        $race = $raceRepository->find(1);
+        $race->getId();
+        $race->getNpcs(10);
+
+        $entity = $race->setName($race->getName())
+            ->setDescription($race->getDescription());
+
+        $errors = $container->get('validator')->validate($entity);
+        $this->assertCount(0, $errors);
+    }
+
+    public function testEntityRemoveNpc(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $raceRepository = $entityManager->getRepository(Race::class);
+        $race = $raceRepository->find(1);
+
+        $npcRepository = $entityManager->getRepository(Npc::class);
+        $npc = $npcRepository->find(10);
+        $race->removeNpc($npc);
+
+        $entity = $race->setName($race->getName())
+            ->setDescription($race->getDescription());
+
+        $errors = $container->get('validator')->validate($entity);
+        $this->assertCount(0, $errors);
+    }
 }
- 
