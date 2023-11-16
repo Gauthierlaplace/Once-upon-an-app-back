15e4a36ed44624f9aa94a0e053ce702aec1912b0
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-11-16 17:20:35 +0100

    usable function on settings, no done, saving today's work

--- src/Controller/api/ItemController.php
+++ src/Controller/api/ItemController.php
@@ -2,26 +2,79 @@
 
 namespace App\Controller\api;
 
+use App\Repository\HeroRepository;
+use App\Repository\ItemRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends CoreApiController
 {
     /**
-     *! Item is used,  
+     *! Item is usable, applies on hero stats and delete item from hero inventory
      *
-     * 
-     * @Route("/api/item/{itemId}", name="app_api_itemUsed" , requirements={"itemId"="\d+"}, methods={"GET"})
+     * @Route("/api/usable/{itemId}", name="app_api_usable" , requirements={"itemId"="\d+"}, methods={"GET"})
      */
-    public function itemUsed($itemId): JsonResponse
+    public function usable($itemId, ItemRepository $itemRepository, HeroRepository $heroRepository): JsonResponse
     {
-        dd($itemId);
+        /** @var App\Entity\User $user */
+        $user = $this->getUser();
+
+        $heroesCollection = $user->getHeroes();
+        $heroes = $heroesCollection->toArray();
+
+        $arrayHero = [];
+        foreach ($heroes as $hero) {
+
+            $itemCollection = $hero->getItem();
+            $items = $itemCollection->toArray();
+
+            foreach ($items as $key => $item) {
+                if ($item->getId() == $itemId) {
+
+                    $itemUsed = $itemRepository->find($itemId);
+                    // TODO check usable function is ok with it, probably this one under ;)
+                    // if ($itemUsed->getHealth() && $itemUsed->getUsable() == true) {
+                    if ($itemUsed->getHealth()) {
+                        $hero->setHealth($hero->getHealth() + $itemUsed->getHealth());
+
+                        if ($hero->getHealth() >= $hero->getMaxHealth()) {
+                            $hero->setHealth($hero->getMaxHealth());
+                        }
+                        unset($items[$key]);
+                        $itemCollection->removeElement($itemUsed);
+                        $hero->removeItem($itemUsed);
+                        $heroRepository->add($hero, true);
+                    }
+                }
+            }
+
+            $arrayHero = [
+                'heroId' => $hero->getId(),
+                'heroName' => $hero->getName(),
+                'heroMaxHealth' => $hero->getMaxHealth(),
+                'health' => $hero->getHealth(),
+                'heroStrength' => $hero->getStrength(),
+                'heroIntelligence' => $hero->getIntelligence(),
+                'heroDexterity' => $hero->getDexterity(),
+                'heroDefense' => $hero->getDefense(),
+                'heroKarma' => $hero->getKarma(),
+                'heroItems' => $items,
+            ];
+        }
+
+        $data = [
+            'player' => $arrayHero,
+        ];
+
+        return $this->json200($data, ["game"]);
     }
 }
 
+// On veut : Utiliser une potion
+
 // On a besoin de : 
-//1. 
-//2.
-//3.
-//4.
+//1. Ajouter un item à un npc dans la table Fight 
+//2. Si le npc meurt, le hero gagne l'item du npc de la table Fight
+//3. Utiliser une potion
+//4. Obtenir l'inventaire du hero (service)
 //5.
