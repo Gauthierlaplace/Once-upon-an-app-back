298fa1da0cc6c48532ad0a038a583285d6d62b60
AnthonyBoutherin <anthony.boutherin@gmail.com>
2023-11-23 11:34:32 +0100

    prep

---
+++ src/Form/SearchType.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('q', TextType::class, [
+            'attr' => [
+                'placeholder' => 'Recherche via un mot clé...'
+            ]
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'method' => 'GET'
+        ])
+    }
+}
